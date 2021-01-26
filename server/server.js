e2eb3c173d6f509a89c811d26fbea71aa77bc05f
ole thomas <norbye93@gmail.com>
2021-01-26 23:05:19 +0100

    hhj

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const WebSocket = require('ws');
 var net = require('net');
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: 8081});
   
 wss.on('connection', ws => {
-  onConnection(ws);
+  
+  onConnection(ws)
   ws.on('message', message => {
     onMessage(message, ws);
   });
@@ -12,17 +13,30 @@ wss.on('connection', ws => {
   });
    ws.on('close', ws=> {
     onClose();
+    
 })
-});
 
-var server = net.createServer();    
-server.on('connection', handleConnection);
-
-server.listen(9000, function() {    
-  console.log('server listening to %j', server.address());  
-});
-
-function handleConnection(conn) {    
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
-  console.log('new client connection from %s', remoteAddress);
+function onClose(){
+  console.log("connection closed")
 }
+
+function onConnection(ws){
+console.log("connected")
+
+wss.emit('message',"hello from server")
+
+};
+
+});
+
+// var server = net.createServer();    
+// server.on('connection', handleConnection);
+
+// server.listen(9000, function() {    
+//   console.log('server listening to %j', server.address());  
+// });
+
+// function handleConnection(conn) {    
+//   var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
+//   console.log('new client connection from %s', remoteAddress);
+// }
---
+++ server/server2.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = 3000;
+
+io.on('connection', socket => {
+  console.log('client connected');
+
+  socket.on('test', (todo) => {
+    console.log(todo)
+
+    io.sockets.emit('test', "hello from server");
+    
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log(' disconnected');
+  });
+});
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+  app.use(bodyParser.json());
+
+  server.listen(port, () => {
+    console.log(`Started on port ${port}`);
+  });
+  
+  module.exports = {app};
