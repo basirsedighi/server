42109889e05e0e4440a42b7e8cbce6d717a8abdc
ole thomas <norbye93@gmail.com>
2021-01-27 15:42:35 +0100

    fe

--- server_fastapi/server.py
+++ server_fastapi/server.py
@@ -1,13 +1,70 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse
+#!/usr/bin/env python
 
-app = FastAPI()
+# WS server example that synchronizes state across clients
+
+import asyncio
+import json
+import logging
+import websockets
+
+logging.basicConfig()
+
+STATE = {"value": 0}
+
+USERS = set()
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        print(data)
-        await websocket.send_text(data)
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+
+async def counter(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            print(data)
+            if data["action"] == "minus":
+                STATE["value"] -= 1
+                await notify_state()
+            elif data["action"] == "plus":
+                STATE["value"] += 1
+                await notify_state()
+            else:
+                logging.error("unsupported event: {}", data)
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(counter, "localhost", 8000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
