1a9df6d869e8d16dd5aefc3497795e39d56e77fa
ole thomas <norbye93@gmail.com>
2021-02-05 20:09:13 +0100

    get

--- server/app.py
+++ server/app.py
@@ -1,18 +1,37 @@
 from typing import List
 
 from fastapi import FastAPI
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, StreamingResponse
 from starlette.websockets import WebSocket, WebSocketDisconnect
 import json
 import queue
 from core.gps import Gps
 import time
+from time import time
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 gpsQueue = queue.Queue()
 
 # gps = Gps(gpsQueue)
 # gps.start()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ConnectionManager:
@@ -37,9 +56,20 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-@app.get('/data/')
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get('/gps')
 async def getData():
-    return "datas"
+    return {"data": "jeeeeo"}
+
+
+@app.route('/')
+def datagdgd():
+
+    return {"data": "message"}
 
 
 @app.get('/storage/')
@@ -47,8 +77,16 @@ async def getData():
     return "datas"
 
 
-def start():
-    # start kamera thread
+def gen():
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.get('/video_feed')
+def video_feed():
+    return StreamingResponse(gen(), media_type="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.websocket("/ws/{socket}")
