499f170dd711c6165b68f4ec7f905287ed826cda
torbis1 <55139121+torbis1@users.noreply.github.com>
2021-02-23 16:40:09 +0100

    tor

--- server/gpiotest_PWM.py
+++ server/gpiotest_PWM.py
@@ -8,24 +8,25 @@ myPWM = GPIO.PWM(18,50)
 myPWM.start(10)
 fps = None
 i = 0
-RestConnect ="http://10.10.10.62:8090/RaspFPS"
+#RestConnect ="http://10.10.10.62:8090/RaspFPS"
 
 while True:
     
-    response = requests.get(RestConnect).text
+    #response = requests.get(RestConnect).text
     #print(response)
+    response = "15"
     if int(response)==0:
         myPWM.ChangeDutyCycle(0)
         if i%100==0:
-            messagePOST = RestConnect+"?FPS=0"
-            requests.post(messagePOST)
+            #messagePOST = RestConnect+"?FPS=0"
+            #requests.post(messagePOST)
     elif int(response)!= fps:
         fps = int(response)
         myPWM.ChangeDutyCycle(50)
         myPWM.ChangeFrequency(int(fps))
     elif fps==int(response) and i%100==0:
-        messagePOST = RestConnect+"?FPS="+str(fps)
-        requests.post(messagePOST)
+        #messagePOST = RestConnect+"?FPS="+str(fps)
+        #requests.post(messagePOST)
         myPWM.ChangeDutyCycle(50)
         myPWM.ChangeFrequency(int(fps))
         
