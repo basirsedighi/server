ffa9d499a82c7f7beeab04d51e2854a2c9605fac
olethomas93 <norbye93@gmail.com>
2021-03-04 15:34:01 +0100

    fe

---
+++ server/pulseorginal.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+myPWM = GPIO.PWM(18,50)
+myPWM.start(10)
+fps = None
+i = 0
+169.254.246.13:8000
+RestConnect ="http://169.254.246.13:8000/RaspFPS"
+
+while True:
+
+    try:
+    
+        response = requests.get(RestConnect).text
+        print(response)
+        #response = "15"
+        if int(response)==0:
+            myPWM.ChangeDutyCycle(0)
+            if i%100==0:
+                print("1")
+                #messagePOST = RestConnect+"?FPS=0"
+                #requests.post(messagePOST)
+        elif int(response)!= fps:
+            fps = int(response)
+            myPWM.ChangeDutyCycle(50)
+            myPWM.ChangeFrequency(int(fps))
+        elif fps==int(response) and i%100==0:
+            #messagePOST = RestConnect+"?FPS="+str(fps)
+            #requests.post(messagePOST)
+            myPWM.ChangeDutyCycle(50)
+            myPWM.ChangeFrequency(int(fps))
+            
+        
+        
+        i=i+1
+    except Exception as e:
+        print(e)
+
+
+GPIO.cleanup()
