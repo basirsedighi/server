9c88f6374bbfb759625c7d2adb53352abf5acbf3
torbis1 <55139121+torbis1@users.noreply.github.com>
2021-03-19 17:36:24 +0100

    merge

--- server/test/test.py
+++ server/test/test.py
@@ -1,6 +1,27 @@
-from datetime import datetime
-dt_obj = datetime.strptime('20.12.2016 09:34:43,76',
-                           '%d.%m.%Y %H:%M:%S,%f')
-millisec = dt_obj.timestamp() * 1000
+from bisect import bisect_left
 
-print(millisec)
+def take_closest(myList, myNumber):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if pos == 0:
+        return myList[0]
+    if pos == len(myList):
+        return myList[-1]
+    before = myList[pos - 1]
+    after = myList[pos]
+    if after - myNumber < myNumber - before:
+       return after
+    else:
+       return before
+
+for n in 
+
+#x = take_closest(lst,k)
+#print(x)      
+#print([item for item, count in collections.Counter(millispiclist).items() if count > 1])   
+
+#print([item for item, count in collections.Counter(millisk1list).items() if count > 1])
