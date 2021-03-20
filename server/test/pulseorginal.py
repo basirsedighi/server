fd41b3a8262f47ac633344d6488be6c4ab5968be
olethomas93 <norbye93@gmail.com>
2021-03-20 14:57:36 +0100

    rgr

--- server/test/pulseorginal.py
+++ server/test/pulseorginal.py
@@ -8,36 +8,33 @@ myPWM = GPIO.PWM(18,50)
 myPWM.start(10)
 fps = None
 i = 0
-RestConnect ="http://192.168.0.100:8000/RaspFPS"
+Restget ="http://192.168.0.100:8000/RaspFPS"
 
 while True:
 
     try:
     
-        response = requests.get(RestConnect)
+        response = requests.get(Restget)
         data = response.json()
         fps_new = data['fps']
         start = data['start']
         print(start)
 
-        if start:
-            if int(fps_new)==0:
-                myPWM.ChangeDutyCycle(0)
-                if i%100==0:
-                    print("1")
-                    #messagePOST = RestConnect+"?FPS=0"
-                    #requests.post(messagePOST)
-            elif int(fps_new)!= fps:
-                fps = int(fps_new)
-                myPWM.ChangeDutyCycle(50)
-                myPWM.ChangeFrequency(int(fps))
-            elif fps==int(fps_new) and i%100==0:
-                #messagePOST = RestConnect+"?FPS="+str(fps)
-                #requests.post(messagePOST)
-                myPWM.ChangeDutyCycle(50)
-                myPWM.ChangeFrequency(int(fps))
-        else:
+        
+        if int(fps_new)==0:
             myPWM.ChangeDutyCycle(0)
+            if i%100==0:
+                print("1")
+               
+        elif int(fps_new)!= fps:
+            fps = int(fps_new)
+            myPWM.ChangeDutyCycle(50)
+            myPWM.ChangeFrequency(int(fps))
+        elif fps==int(fps_new) and i%100==0:
+           
+            myPWM.ChangeDutyCycle(50)
+            myPWM.ChangeFrequency(int(fps))
+        
 
                 
         
@@ -46,6 +43,6 @@ while True:
     except Exception as e:
 
         myPWM.ChangeDutyCycle(50)
-        myPWM.ChangeFrequency(5)
+        myPWM.ChangeFrequency(0)
 
 GPIO.cleanup()
