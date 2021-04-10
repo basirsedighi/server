c211f4ca3d0457613385641bc896b879d7046b5a
olethomas93 <norbye93@gmail.com>
2021-04-10 12:08:43 +0200

    frf

--- server/core/camera.py
+++ server/core/camera.py
@@ -9,7 +9,7 @@ class Camera:
         self.image = None
         self.device = None
         self.stream = None
-        self.config_path ="core/config/conf1.gcs"
+        self.config_path ="core/config/calconf.gcs"
         self.tempimg = None
         
 
