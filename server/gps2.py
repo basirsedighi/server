db3517cded7c57655ab16e236e4665bc85eb2bdc
olethomas93 <norbye93@gmail.com>
2021-04-13 13:11:31 +0200

    fdf

--- server/gps2.py
+++ server/gps2.py
@@ -165,10 +165,11 @@ class gpsHandler(Thread):
         fix_quality = 0
         hdop = 5
         velocity = 0
+        sys.stderr.write(now)
+
         
         if(msg.sentence_type =="RMC"):
-            print("validity")
-            print(msg.status)
+           
             if msg.status =="A":
                 velocity = self.__knotsToKmh(msg.spd_over_grnd)
                 velocity = self.__kmhToMs(velocity)
