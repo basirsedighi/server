6c0b0a68d0b3f4b6cf814c680dc1d9c129e7bd8f
olethomas93 <norbye93@gmail.com>
2021-04-14 11:50:19 +0200

    fgfg

--- server/core/camera.py
+++ server/core/camera.py
@@ -9,7 +9,7 @@ class Camera:
         self.image = None
         self.device = None
         self.stream = None
-        self.config_path ="core/config/calconf.gcs"
+        self.config_path ="core/config/pulseConf.gcs"
         self.tempimg = None
         
 
