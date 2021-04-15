3dd6ed4812e983f90f8ab4dac5dde7c43b65db17
olethomas93 <norbye93@gmail.com>
2021-04-15 11:29:14 +0200

    dfd

--- server/core/camera.py
+++ server/core/camera.py
@@ -48,7 +48,7 @@ class Camera:
         try:
            
             self.device = device
-            test =self.device.read_property(cvb.DiscoveryProperties.UsbPortPath )
+            test =self.device.read_property(cvb.DiscoveryProperties.DeviceId )
 
             print(test)
        
