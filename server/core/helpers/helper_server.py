7fdf71d3f9a467686fb5bf23105c55a99a3dc6b5
olethomas93 <norbye93@gmail.com>
2021-04-15 11:05:23 +0200

    gfg

--- server/core/helpers/helper_server.py
+++ server/core/helpers/helper_server.py
@@ -295,6 +295,7 @@ def folderConstructor():
 async def discoverCameras():
 
     discover = cvb.DeviceFactory.discover_from_root(cvb.DiscoverFlags.IgnoreVins)
+    print(discover)fdf
     
     return len(discover)
 
