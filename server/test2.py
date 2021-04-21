d021b222f08f832e9b72bfebbd3c32229ca9ebd7
olethomas93 <norbye93@gmail.com>
2021-04-21 19:34:41 +0200

    jkj

--- server/test2.py
+++ server/test2.py
@@ -4,13 +4,19 @@ import os
 def fixPath(path):
 
     test = path.split("\\")
-    test.pop()
-    newPath = '/'.join(test)
-    return newPath
+    if len(test)>0:
+
+        test.pop()
+        newPath = '/'.join(test)
+        return newPath
+    else:
+        return path
 
 absolute_path = os.path.dirname(os.path.abspath(__file__))
 
 test = fixPath(absolute_path)
+
+
 
 path = test+"/log/"
 
