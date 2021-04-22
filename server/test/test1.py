28ee27e7c33fe9c73779da51770e52792642af64
olethomas93 <norbye93@gmail.com>
2021-04-22 11:56:21 +0200

    dfd

--- server/test/test1.py
+++ server/test/test1.py
@@ -1,5 +1,39 @@
-import datetime
+import os
 
-x  = datetime.datetime.now()
 
-print(x.hour )
+
+
+
+
+
+
+
+
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+path = absolute_path+"/log/"+date+"/"+tempTrip
+
+
+
+# Open image csv file and make a csv reader object 
+with open(path +'/images.csv', newline='') as csvpic:
+    picreader = csv.reader(csvpic, delimiter=',', quotechar='|')
+    for row in picreader:
+        #  make a list for each column in the csv file
+        picnumberlist.append(row[0])                
+        millisk1list.append(float(row[3]))
+        
+        
+            
+            
+    
+# Open gps csv file and make a csv reader object 
+with open(path +'/gps.csv', newline='') as csvgps:
+    gpsreader = csv.reader(csvgps, delimiter=',', quotechar='|')
+    for row in gpsreader:
+        #  make a list for each column in the csv file
+        fixlist.append(row[1])
+        speedlist.append(float(row[2]))
+        millisgpslist.append(float(row[4])) 
+        latlist.append(float(row[5]))
+        longlist.append(float(row[6]))
+        
