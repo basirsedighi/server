30036c08ec1767a290e23f2d9440a1f307f6799d
olethomas93 <norbye93@gmail.com>
2021-04-26 14:41:53 +0200

    fefe

--- server/gps2.py
+++ server/gps2.py
@@ -323,8 +323,7 @@ class gpsHandler(Thread):
         self.send('SSSSSSSSSS\r\n')
         self.send('setDataInOut, COM3, CMD, SBF+NMEA\r\n')
         self.send('setDataInOut, COM2, RTCMv3, SBF+NMEA\r\n')
-        self.send('SetNMEAOutput, Stream1, COM2, GGA, msec40\r\n')
-        self.send('SetNMEAOutput, Stream7, COM3, GSV+GSA, msec40\r\n')
+        self.send('SetNMEAOutput, Stream1, COM2, GGA, sec1\r\n')
         self.send('SetNMEAOutput, Stream8, COM3, GGA+VTG+RMC, msec40\r\n')
         
   
