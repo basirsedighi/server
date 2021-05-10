05144113e07a65270bd83d9fb68b58430b5a9a02
olethomas93 <norbye93@gmail.com>
2021-05-10 17:44:07 +0200

    hkj

--- server/core/imageSave.py
+++ server/core/imageSave.py
@@ -13,7 +13,7 @@ import json
 from core.helpers.helper_server import most_free_space
 
 
-class ImageSave(Process()):
+class ImageSave(Process):
     def __init__(self, imageQueue,name):
         Process.__init__(self)
         self.daemon = True
